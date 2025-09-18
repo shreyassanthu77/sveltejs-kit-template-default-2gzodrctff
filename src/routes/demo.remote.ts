b3991d59750dec3a64328fb7d04356dd92eb3687
shreyassanthu77 <shreyassanthu77@gmail.com>
2025-09-18 22:52:46 +0530

    commit

--- src/routes/demo.remote.ts
+++ src/routes/demo.remote.ts
@@ -2,13 +2,8 @@ import { form } from '$app/server';
 import * as z from 'zod';
 
 export const demo = form(
-  z.object({
-    name: z.object({
-      first: z.string(),
-      last: z.string(),
-    }),
-  }),
-  ({ name }) => {
-    console.log(name);
+  "unchecked",
+  (data) => {
+    console.log(data);
   }
 );
